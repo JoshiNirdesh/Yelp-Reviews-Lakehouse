b6544b22ace46e25a04993f39f419678faf579e1
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-04-06 17:08:11 +0545

    bronze to silver

--- src/bronze_to_silver.py
+++ src/bronze_to_silver.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 import pandas as pd
-from pyspark.sql.functions import col,trim
+from pyspark.sql.functions import col,trim,to_timestamp,when,split,size
 spark = SparkSession.builder.appName("practise").getOrCreate()
 
 business = spark.read.parquet("../data/bronze/business")
@@ -30,5 +30,65 @@ business_silver.write.mode("overwrite").parquet("../data/silver/business")
 
 #checkin
 checkin = spark.read.parquet("../data/bronze/checkin")
+checkin_silver = (
+    checkin
+    .dropDuplicates(["business_id"])
+    .filter(col("business_id").isNotNull())
+    .withColumn("date", to_timestamp("date"))
+    )
+# checkin_silver.printSchema() 
 
-checkin.printSchema()
+#reviews
+
+reviews = spark.read.parquet("../data/bronze/reviews")
+
+reviews_silver = (
+    reviews
+    .dropDuplicates(["review_id"])
+    .filter(col("review_id").isNotNull())
+    .filter(col("business_id").isNotNull())
+    .filter(col("user_id").isNotNull())
+    .withColumn("cool", col("cool").cast("int"))
+    .withColumn("funny",col("funny").cast("int"))
+    .withColumn("stars",col("stars").cast("double"))
+    .withColumn("useful",col("useful").cast("int"))
+    .filter(((col("stars")>=1)&(col("stars")<=5)))   
+)
+
+# reviews.show()
+# reviews.printSchema()
+
+reviews_silver.write.mode("overwrite").parquet("../data/silver/reviews")
+
+#tip
+tip = spark.read.parquet("../data/bronze/tip")
+
+
+
+tip_silver = (
+    tip
+    .drop_duplicates(["business_id"])
+    .filter(col("business_id").isNotNull())
+    .filter(col("user_id").isNotNull())
+    .withColumn("compliment_count",col("compliment_count").cast("int"))
+)
+# tip.printSchema()
+# tip.show()
+
+tip_silver.write.mode("overwrite").parquet("../data/siver/tip")
+
+user = spark.read.parquet("../data/bronze/user")
+
+user_silver = (
+    user
+    .dropDuplicates(["user_id"])
+    .filter(col("user_id").isNotNull())
+    .withColumn(
+        "elite_list",
+        when(col("elite").isNull() | (col("elite") == ""), None).otherwise(split(col("elite"), ","))
+    )
+    .withColumn("elite_years_count", size(col("elite_list")))
+
+# )
+# user.printSchema()
+# user.show()
